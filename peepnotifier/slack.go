2b7bbdbb83438510f60ec3b6fe971e714b7963d6
Songmu <y.songmu@gmail.com>
2018-12-31 22:06:35 +0900

    slack notifier accepts channel via flag

--- peepnotifier/slack.go
+++ peepnotifier/slack.go
@@ -1,26 +1,43 @@
 package peepnotifier
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	slacli "github.com/monochromegane/slack-incoming-webhooks"
 )
 
 type slack struct {
+	channel string
 }
 
 func (sl *slack) run(re *result, args []string) error {
+	fs := flag.NewFlagSet("peepnotify slack", flag.ContinueOnError)
+	fs.StringVar(&sl.channel, "c", "", "slack channel")
+
+	err := fs.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	if sl.channel != "" && !strings.HasPrefix(sl.channel, "#") {
+		sl.channel = "#" + sl.channel
+	}
+
 	u := os.Getenv("SLACK_WEBHOOK_URL")
 	if u == "" {
 		return fmt.Errorf("please specify environment variable `SLACK_WEBHOOK_URL`")
 	}
 	cli := slacli.Client{WebhookURL: u}
 	msg := fmt.Sprintf(
-		"The command %q by %s is finished around %s, which started at %s on %s",
-		re.Command, re.User, re.Ended, re.Started, re.Host)
+		"The command %q on %s by %s is finished around %s, which started at %s",
+		re.Command, re.Host, re.User, re.Ended, re.Started)
 	return cli.Post(&slacli.Payload{
-		Username: "peepnotifier",
-		Text:     msg,
+		Username:  "peepnotifier",
+		Text:      msg,
+		IconEmoji: ":white_flower:",
+		Channel:   sl.channel,
 	})
 }
