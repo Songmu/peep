9a2ba7f97a5620c515bbc8e02291e435626bf3e8
Songmu <y.songmu@gmail.com>
2018-12-31 17:11:50 +0900

    arrange files

--- peepnotifier/peepnotifier.go
+++ peepnotifier/peepnotifier.go
@@ -1,10 +1,13 @@
 package peepnotifier
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"time"
 )
 
 var (
@@ -19,7 +22,7 @@ const (
 
 // Run the peepnotifier
 func Run(args []string) int {
-	err := (&cli{outStream: os.Stdout, errStream: os.Stderr}).run(args)
+	err := (&peepnotifier{outStream: os.Stdout, errStream: os.Stderr}).run(args)
 	if err != nil {
 		if err == flag.ErrHelp {
 			return exitCodeOK
@@ -28,4 +31,52 @@ func Run(args []string) int {
 		return exitCodeErr
 	}
 	return exitCodeOK
+}
+
+type peepnotifier struct {
+	outStream, errStream io.Writer
+}
+
+func (pe *peepnotifier) run(args []string) error {
+	outStream = pe.outStream
+	errStream = pe.errStream
+
+	log.SetOutput(pe.errStream)
+	log.SetPrefix("[peepnotify] ")
+	log.SetFlags(0)
+
+	return pe.dispatch(args)
+}
+
+var runnerMap = map[string]runner{
+	"stdout":     &stdout{},
+	"pushbullet": &pushbullet{},
+}
+
+func (pe *peepnotifier) dispatch(args []string) error {
+	if len(args) < 1 {
+		return fmt.Errorf("no subcommands specified")
+	}
+	ru, ok := runnerMap[args[0]]
+	if !ok {
+		return fmt.Errorf("unknown subcommand: %s", args[0])
+	}
+	re := &result{}
+	err := json.NewDecoder(os.Stdin).Decode(re)
+	if err != nil {
+		return err
+	}
+	return ru.run(re, args[1:])
+}
+
+type runner interface {
+	run(*result, []string) error
+}
+
+type result struct {
+	User    string    `json:"user"`
+	Command string    `json:"command"`
+	Started time.Time `json:"started"`
+	Ended   time.Time `json:"ended"`
+	Host    string    `json:"host"`
 }
