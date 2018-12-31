122e45fbe046c780a498decdff574d64e207ac79
Songmu <y.songmu@gmail.com>
2018-12-31 23:42:47 +0900

    add peepnotifier/stdout_test.go

---
+++ peepnotifier/stdout_test.go
@@ -0,0 +1,37 @@
+package peepnotifier
+
+import (
+	"bytes"
+	"encoding/json"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestStdout_run(t *testing.T) {
+	st := &stdout{}
+	re := &result{
+		User:    "Songmu",
+		Command: `perl -E 'say "Hello"'`,
+		Host:    "localhost",
+		Started: time.Now().UTC(),
+		Ended:   time.Now().UTC(),
+	}
+	var (
+		buf  = &bytes.Buffer{}
+		orig = outStream
+	)
+	outStream = buf
+	defer func() { outStream = orig }()
+
+	st.run(re, nil)
+	out := &result{}
+	err := json.NewDecoder(buf).Decode(out)
+	if err != nil {
+		t.Errorf("err should be nil but: %s", err)
+	}
+
+	if !reflect.DeepEqual(re, out) {
+		t.Errorf("something went wrong")
+	}
+}
